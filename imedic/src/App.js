419558cab10fa71b6213811b7155b03c522664a2
Pere Busquet <perebusquet87gmail.com>
2018-10-13 09:21:13 +0200

    navbar

--- imedic/src/App.js
+++ imedic/src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import {BrowserRouter} from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         Hola Joder
+        <Sidebar />
+        <div id="page-wrap">
+          <h1>IMEDIC</h1>
+          <h2>Check out our tutorials the side menubar</h2>
+        </div>
       </div>
+      </BrowserRouter> 
     );
   }
 }
 
 export default App;
+
+
+
+
+
