4d3803d64cb4062e2509302c7db7c3b244e4d848
Pere Busquet <perebusquet87gmail.com>
2018-10-15 18:04:03 +0200

    edit

---
+++ imedic/src/pages/EditPatient.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Form from '.components./Form';
+ class EditPatient extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Edit</h1>
+        <Form/>
+      </div>
+    )
+  }
+}
+export default EditPatient;
