4a17511683440e6b592280dbd981f9c7433878ab
Pere Busquet <perebusquet87gmail.com>
2018-10-15 19:47:58 +0200

    home

--- imedic/src/pages/Private.js
+++ imedic/src/pages/Private.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Search from '../components/Search';
+import FormPatient from '../components/FormPatient';
 
 export default class Private extends Component {
   render() {
     return (
       <div>
+      <div>
         Private
+        </div>
+        <div>
+        <Search/>
+        <FormPatient/>
+      </div>
       </div>
     )
   }
