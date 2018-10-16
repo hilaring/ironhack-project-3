847d2f4031bf3f962602b9f34b63e65809e7930c
Pere Busquet <perebusquet87gmail.com>
2018-10-16 15:37:44 +0200

    pages

--- imedic/src/pages/Detail.js
+++ imedic/src/pages/Detail.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom';
-
+import Patient from 'data';
 export default class Detail extends Component {
   
   state = {
