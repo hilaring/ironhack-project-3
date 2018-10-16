31c13f2a22650b45c1836b2ba8a7de021bc9d70b
Pere Busquet <perebusquet87gmail.com>
2018-10-16 15:34:50 +0200

    detail + home

---
+++ imedic/src/pages/Detail.js
@@ -0,0 +1,49 @@
+import React, { Component, Fragment } from 'react'
+import { Link } from 'react-router-dom';
+
+export default class Detail extends Component {
+  
+  state = {
+    patient: {},
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params; 
+    Data.getpatient(id)
+      .then(({data}) => {
+        this.setState({
+          patient: data,
+          isLoading: false,
+        })
+      })
+  }
+
+  renderInfo = () => {
+    const { name, last_name, email,number,adress } = this.state.patient;
+    return (
+      <Fragment>
+        <h3>Name</h3>
+        <p>{name}</p>
+        <h3>Last Name</h3>
+        <p>{last_name}</p>
+        <h3>Email</h3>
+        <p>{email}</p>
+        <h3>Phone Number</h3>
+        <p>{number}</p>
+        <h3>Adress</h3>
+        <p>{adress}</p>
+        <Link to="/">Back to home</Link>
+      </Fragment>
+    );
+  }
+
+  render() {
+    
+    return (
+      <div>
+        { !this.state.isLoading ? this.renderInfo() : <div>Loading</div>}
+      </div>
+    )
+  }
+}
---
+++ imedic/src/pages/Home.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+class Home extends Component {
+
+  state = {
+    patient: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    Data.getpatient()
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          patient: data,
+          isLoading: false
+        })
+      })
+      .catch((error) => {
+
+      })
+  }
+
+  renderList = () => {
+    return this.state.patient.map(({ name, last_name, _id }) => 
+      <li key={_id}>
+        {name} - <Link to={`/patient/${_id}`}>{last_name}</Link> 
+      </li>)
+  }
+
+  render() {   
+    return (
+      <div>
+        <h1>Listado de Pacientes</h1>
+        <ul>
+          {this.state.isLoading ? <h1>Loading</h1> : this.renderList()}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Home;
