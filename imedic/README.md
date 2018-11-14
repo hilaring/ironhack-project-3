3c8933d2677d5a7441e55be39d492325dc8b8629
Pere Busquet <perebusquet87gmail.com>
2018-11-14 16:36:32 +0100

    ok

--- imedic/README.md
+++ imedic/README.md
@@ -1,4 +1,4 @@
-# Iron Healt
+# Imedic
 
 
 
--- imedic/src/components/Title.js
+++ imedic/src/components/Title.js
@@ -5,7 +5,7 @@ const Heading = styled.div `
   color: #f2f2f2;
   font-size: 26px;
   text-align: center;
-  padding-top 15px;
+  padding-top :15px;
   padding-bottom: 5px;
   justify-content: middle;
   background-color: #6D7AE0;
@@ -16,7 +16,7 @@ const Heading = styled.div `
 class Title extends Component {
   render() {
     return (
-      <Heading>
+      <Heading className='heading'>
         i<b>m</b>edic
       </Heading>
     )
